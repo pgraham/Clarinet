92eb00225393ae2cd9e43244589d62bb562f5356
Philip Graham <philip.robert.graham@gmail.com>
2014-07-31 15:30:25 +0000

    [minor] CS Fixes

--- src/model/ModelCache.php
+++ src/model/ModelCache.php
@@ -5,7 +5,7 @@
  * All rights reserved.
  *
  * This file is part of Clarinet and is licensed by the Copyright holder under
- * the 3-clause BSD License.  The full text of the license can be found in the
+ * the 3-clause BSD License. The full text of the license can be found in the
  * LICENSE.txt file included in the root directory of this distribution or at
  * the link below.
  * =============================================================================
@@ -14,11 +14,11 @@
  */
 namespace zpt\orm\model;
 
-use \zpt\orm\model\Model;
-use \zpt\orm\model\parser\ModelParser;
+use zpt\orm\model\Model;
+use zpt\orm\model\parser\ModelParser;
 
 /**
- * This class stores reference to parsed models.  Note that if it is possible
+ * This class stores reference to parsed models. Note that if it is possible
  * that a model in the cache is in an incomplete state due to its relationships
  * having not been fully parsed.
  *
@@ -26,31 +26,31 @@ use \zpt\orm\model\parser\ModelParser;
  */
 class ModelCache {
 
-  private $cache = array();
+	private $cache = array();
 
-  private $modelParser;
+	private $modelParser;
 
-  public function clear() {
-    $this->cache = array();
-  }
+	public function clear() {
+		$this->cache = array();
+	}
 
-  public function get($className) {
-    if (!$this->isCached($className)) {
-      $this->modelParser->parse($className);
-    }
+	public function get($className) {
+		if (!$this->isCached($className)) {
+			$this->modelParser->parse($className);
+		}
 
-    return $this->cache[$className];
-  }
+		return $this->cache[$className];
+	}
 
-  public function isCached($className) {
-    return isset($this->cache[$className]);
-  }
+	public function isCached($className) {
+		return isset($this->cache[$className]);
+	}
 
-  public function set($className, Model $model) {
-    $this->cache[$className] = $model;
-  }
+	public function set($className, Model $model) {
+		$this->cache[$className] = $model;
+	}
 
-  public function setModelParser(ModelParser $modelParser) {
-    $this->modelParser = $modelParser;
-  }
+	public function setModelParser(ModelParser $modelParser) {
+		$this->modelParser = $modelParser;
+	}
 }
