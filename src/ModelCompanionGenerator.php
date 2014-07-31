045b7125551432ffe4d67802f1380948d5d858b6
Philip Graham <philip.robert.graham@gmail.com>
2014-07-31 15:24:24 +0000

    [minor] CS fixes

--- src/ModelCompanionGenerator.php
+++ src/ModelCompanionGenerator.php
@@ -5,7 +5,7 @@
  * All rights reserved.
  *
  * This file is part of Clarinet and is licensed by the Copyright holder under
- * the 3-clause BSD License.  The full text of the license can be found in the
+ * the 3-clause BSD License. The full text of the license can be found in the
  * LICENSE.txt file included in the root directory of this distribution or at
  * the link below.
  * =============================================================================
@@ -14,12 +14,12 @@
  */
 namespace zpt\orm;
 
-use \zpt\orm\model\Model;
-use \zpt\opal\CompanionGenerator;
-use \zpt\orm\model\ModelCache;
+use zpt\orm\model\Model;
+use zpt\opal\CompanionGenerator;
+use zpt\orm\model\ModelCache;
 
 /**
- * This class provides functionality common to all persisters.  This includes
+ * This class provides functionality common to all persisters.	This includes
  * Parsing the model and output the generated code to a specified output
  * directory.
  *
@@ -27,29 +27,29 @@ use \zpt\orm\model\ModelCache;
  */
 abstract class ModelCompanionGenerator extends CompanionGenerator {
 
-  private $modelCache;
+	private $modelCache;
 
-  public function setModelCache(ModelCache $modelCache) {
-    $this->modelCache = $modelCache;
-  }
+	public function setModelCache(ModelCache $modelCache) {
+		$this->modelCache = $modelCache;
+	}
 
-  /**
-   * Generate the code.  This method delegates to the implementation for the
-   * acutal generation then outputs to the specified path.
-   *
-   * @param string $className The entity for which to generate code.
-   */
-  protected function getValues($className) {
-    $model = $this->modelCache->get($className);
-    return $this->getValuesForModel($model);
-  }
+	/**
+	 * Generate the code.  This method delegates to the implementation for the
+	 * acutal generation then outputs to the specified path.
+	 *
+	 * @param string $className The entity for which to generate code.
+	 */
+	protected function getValues($className) {
+		$model = $this->modelCache->get($className);
+		return $this->getValuesForModel($model);
+	}
 
-  /**
-   * This method is responsible for actually generating the actor code.
-   *
-   * @param Model $model Parsed model information.
-   * @return string The PHP code for the generated actor.
-   */
-  protected abstract function getValuesForModel(Model $model);
+	/**
+	 * This method is responsible for actually generating the actor code.
+	 *
+	 * @param Model $model Parsed model information.
+	 * @return string The PHP code for the generated actor.
+	 */
+	protected abstract function getValuesForModel(Model $model);
 
 }
