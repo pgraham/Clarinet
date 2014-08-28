3f51452a825eabec1a081af143bcb82b5d18333f
Philip Graham <philip.robert.graham@gmail.com>
2014-08-28 14:53:34 +0000

    Added initial version of Repository class

---
+++ src/Repository.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * Copyright (c) 2014, Philip Graham
+ * All rights reserved.
+ *
+ * This file is part of Clarinet. For the full copyright and license information
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace zpt\orm;
+
+use zpt\db\DatabaseConnection;
+use zpt\opal\CompanionLoaderFactory;
+use zpt\opal\Psr4Dir;
+use zpt\orm\companion\PersisterCompanionDirector;
+use zpt\orm\model\ModelFactory;
+
+/**
+ * This class encapsulates the entities available through a single database
+ * connection.
+ *
+ * @author Philip Graham <philip@zeptech.ca>
+ */
+class Repository
+{
+
+	private $db;
+
+	private $persisterLdr;
+	private $transformerLdr;
+	private $validatorLdr;
+	private $msgGenLdr;
+	private $qbLdr;
+
+	/**
+	 * Create a new Repository representing entities stored in the specified
+	 * database.
+	 *
+	 * @param DatabaseConnection $db
+	 * @param Psr4Dir $dynTarget
+	 *   The PSR-4 prefix target directory where generated Companion sources are
+	 *   found.
+	 */
+	public function __construct(DatabaseConnection $db, Psr4Dir $dynTarget) {
+		$this->db = $db;
+
+		$modelFactory = new ModelFactory();
+		$persisterCompDir = new PersisterCompanionDirector($modelFactory);
+
+		$ldrFactory = new CompanionLoaderFactory($dynTarget);
+		$this->persisterLdr = $ldrFactory->get($persisterCompDir);
+	}
+
+	public function getPersister($model) {
+		return $this->persisterLdr->get($model);
+	}
+
+	public function getTransformer($model) {
+
+	}
+
+	public function getMessageGenerator($model) {
+
+	}
+}
---
+++ test/unit/RepositoryTest.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Copyright (c) 2014, Philip Graham
+ * All rights reserved.
+ *
+ * This file is part of Clarinet. For the full copyright and license information
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace zpt\orm;
+
+/**
+ * This class tests the {@link Repository} class.
+ *
+ * @author Philip Graham <philip@zeptech.ca>
+ */
